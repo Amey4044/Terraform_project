f11822516a9b33a5b71ae45b876f6dbd94b642da
Amey4044 <ameydeshmukh1111@gmail.com>
2025-09-29 04:05:36 +0530

    ok

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,52 +1,52 @@
-provider "null" {}
+variable "prebuilt_box_path" {
+  default = "C:\\Users\\AMEY\\Desktop\\terraform-gcp-ansible\\terraform\\prebuilt_box"
+}
 
+locals {
+  vms = ["lb", "web1", "web2", "db"]
+}
+
+# -------------------------
+# Provision VMs with Vagrant in parallel
+# -------------------------
 resource "null_resource" "vagrant_multi_vm" {
-  count = length(var.vms)
+  count = length(local.vms)
+
+  triggers = {
+    vm_name = local.vms[count.index]
+  }
 
   provisioner "local-exec" {
-    command = <<EOT
-      cd prebuilt_box
-      vagrant up ${var.vms[count.index].name} --provider=virtualbox
-    EOT
+    command     = "start /b cmd /c \"cd /d ${var.prebuilt_box_path} && vagrant up ${local.vms[count.index]} --provider=virtualbox --provision\""
+    interpreter = ["cmd", "/c"]
   }
 }
 
+# -------------------------
+# Configure VMs after boot in parallel
+# -------------------------
 resource "null_resource" "vm_configure" {
-  count = length(var.vms)
+  count = length(local.vms)
 
-  depends_on = [null_resource.vagrant_multi_vm]
+  triggers = {
+    vm_name = local.vms[count.index]
+  }
 
   provisioner "local-exec" {
-    command = <<EOT
-      vagrant ssh ${var.vms[count.index].name} -c "
-        if [[ '${var.vms[count.index].type}' == 'loadbalancer' ]]; then
-          sudo tee /etc/nginx/sites-available/loadbalancer <<EOF
-upstream backend {
-    server 192.168.56.101;
-    server 192.168.56.102;
+    command     = "start /b cmd /c \"cd /d ${var.prebuilt_box_path} && vagrant ssh ${local.vms[count.index]} -c \\\"echo VM ${local.vms[count.index]} is up\\\"\""
+    interpreter = ["cmd", "/c"]
+  }
 }
 
-server {
-    listen 80;
-    location / {
-        proxy_pass http://backend;
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-    }
-}
-EOF
-          sudo ln -sf /etc/nginx/sites-available/loadbalancer /etc/nginx/sites-enabled/loadbalancer
-          sudo rm -f /etc/nginx/sites-enabled/default
-          sudo systemctl restart nginx
-        elif [[ '${var.vms[count.index].type}' == 'web' ]]; then
-          echo '${var.vms[count.index].message}' | sudo tee /var/www/html/index.html
-          sudo systemctl start nginx
-        elif [[ '${var.vms[count.index].type}' == 'db' ]]; then
-          sudo systemctl start mysql
-          sudo mysql -e \"ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root'; FLUSH PRIVILEGES;\"
-        fi
-      "
-    EOT
-  }
+# -------------------------
+# Output VM Info
+# -------------------------
+output "vagrant_vm_info" {
+  value = <<EOT
+Load Balancer:  http://localhost:8083  (SSH: localhost:2223)
+Web1:           http://localhost:8084  (SSH: localhost:2224)
+Web2:           http://localhost:8086  (SSH: localhost:2225)
+Database:       SSH: localhost:2230
+Private IPs:    LB=192.168.56.5, Web1=192.168.56.101, Web2=192.168.56.102, DB=192.168.56.20
+EOT
 }
